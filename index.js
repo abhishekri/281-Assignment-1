60a9a8a88e1c8d74bad51bcd6b2896dc86a668c0
Abhishek <abhishekairspace@gmail.com>
2022-02-16 00:51:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ http.createServer(function (request, response) {
            response.writeHead(200, {'Content-Type': 'text/plain'});
 
            response.end('Hello World - Application is Working \n');         
-        
+           
+           document.body.innerText = String.fromCodePoint(0x1F354);
 
 }).listen(8080);
 
